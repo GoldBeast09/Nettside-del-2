1a6fd201601b6e359053f97746be25a0bd8208d3
Anders <anders.sovik@outlook.com>
2026-01-06 19:38:54 +0100

    fixed a type error

--- JS/Ham-Menu.js
+++ JS/Ham-Menu.js
@@ -1,15 +1,14 @@
-const hamMenu = document.querySelector('.ham-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu')
+const hamMenu = document.querySelector(".ham-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
 
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
 
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active')
-    offScreenMenu.classList.toggle('active')
-})
-
-document.addEventListener('click', B => {
-    if (!hamMenu.contains(B.target) && !offScreenMenu.contains(B.target)) {
-        hamMenu.classList.remove('active')
-        offScreenMenu.classList.remove('active')
-    }
-})
+document.addEventListener("click", (Body) => {
+  if (!hamMenu.contains(Body.target) && !offScreenMenu.contains(Body.target)) {
+    hamMenu.classList.remove("active");
+    offScreenMenu.classList.remove("active");
+  }
+});
