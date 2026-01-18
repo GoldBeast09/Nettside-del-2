0ce07bc2d634be0c271cbc841e43c6c130078457
Anders <anders.sovik@outlook.com>
2026-01-18 18:39:11 +0100

    Fixed some css, formated code and started logic for the quiz

---
+++ JS/Quiz.js
@@ -0,0 +1,27 @@
+const QuestionElement = document.getElementsByClassName("question");
+const correctAnswers = document.getElementsByClassName("correctAnswers");
+const totalQuestions = document.getElementsByClassName("totalQuestions");
+const Options = document.querySelectorAll(".Option");
+
+const questions = [
+  {
+    question: "Placeholder one",
+    answers: ["option1", "option2", "option3", "option4"],
+    corectIndex: 1,
+  },
+  {
+    question: "Placeholder two",
+    answers: ["option1", "option2", "option3", "option4"],
+    corectIndex: 2,
+  },
+  {
+    question: "Placeholder three",
+    answers: ["option1", "option2", "option3", "option4"],
+    corectIndex: 3,
+  },
+  {
+    question: "Placeholder four",
+    answers: ["option1", "option2", "option3", "option4"],
+    corectIndex: 4,
+  },
+];
