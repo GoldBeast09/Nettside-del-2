cc8904dc264adda6a963c43b3a2b80a38a612122
Anders <anders.sovik@outlook.com>
2026-01-21 20:52:47 +0100

    Compared answers

--- JS/Quiz.js
+++ JS/Quiz.js
@@ -7,43 +7,64 @@ const questions = [
   {
     question: "Placeholder one",
     answers: ["option1", "option2", "option3", "option4"],
-    corectIndex: 1,
+    corectIndex: 0,
   },
   {
     question: "Placeholder two",
     answers: ["option1", "test", "option3", "option4"],
-    corectIndex: 2,
+    corectIndex: 1,
   },
   {
     question: "Placeholder three",
     answers: ["option1", "test", "option3", "option4"],
-    corectIndex: 3,
+    corectIndex: 2,
   },
   {
     question: "Placeholder four",
     answers: ["option1", "option2", "option3", "option4"],
-    corectIndex: 4,
+    corectIndex: 3,
   },
   {
     question: "Placeholder 5",
     answers: ["option1", "option2", "option3", "option4"],
-    corectIndex: 1,
+    corectIndex: 0,
   },
 ];
 
 let correctAnswers = 0;
 let totalQuestions = questions.length;
 let answeredQuestions = 0;
+//Må komme på et bedre navn senere
+let currentQuestionForNow = 0;
 
 correctAnswersElement.textContent = `${correctAnswers}/${answeredQuestions} Correct`;
 totalQuestionsElement.textContent = `Question ${answeredQuestions}/${totalQuestions}`;
+
+const randomQuestion = () => {
+  return Math.floor(Math.random() * questions.length);
+};
+
+updateUi(randomQuestion());
 
 function updateUi(currentQuestion) {
   optionsElement.forEach((button, index) => {
     const optionText = button.querySelector(".optionText");
     optionText.textContent = questions[currentQuestion].answers[index];
+
+    questionElement.textContent = questions[currentQuestion].question;
+
+    button.addEventListener("click", () => {
+      compareAnswers(index, currentQuestion);
+    });
   });
-  questionElement.textContent = questions[currentQuestion].question;
+}
+
+function compareAnswers(answer, question) {
+  if (answer === questions[question].corectIndex) {
+    console.log("correct");
+  } else {
+    console.log("wrong");
+  }
 }
 
 /*
